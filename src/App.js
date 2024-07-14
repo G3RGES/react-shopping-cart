eff3649f9230b8c866752e7c5783137665a1e431
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-14 03:28:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element="" />
+          <Route path="/cart" element="" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
