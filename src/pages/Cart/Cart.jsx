5b53302080b15651bdeaf954acc35a12d958bc44
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-15 02:32:26 +0300

    Update Cart.jsx

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PRODUCTS } from "../../Products";
+
+import CartItem from "./CartItem";
+import { useNavigate } from "react-router-dom";
+
 import "./cart.css";
 
 const Cart = () => {
-  return <div className="cart">Cart</div>;
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  // const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="cart">
+      <div>
+        <h1 className="">Your Cart Items</h1>
+      </div>
+
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
