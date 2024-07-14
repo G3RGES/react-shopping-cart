a0dda7786c711172cf3a1217ae11a077aa3e7d48
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-14 05:26:05 +0300

    Update Product.jsx

--- src/pages/Shop/Product.jsx
+++ src/pages/Shop/Product.jsx
@@ -1,27 +1,23 @@
 import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
-const Product = ({ name, price, image }) => {
-  //   const { addToCart, cartItems } = useContext(ShopContext);
+const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
 
-  // const cartItemCount = cartItems[id];
+  const cartItemCount = cartItems[id];
 
   return (
     <div className="product">
-      <img src={image} alt={name} />
+      <img src={productImage} alt={productImage} />
       <div className="description">
         <p>
-          <b>{name}</b>
+          <b>{productName}</b>
         </p>
         <p> ${price}</p>
       </div>
-      <button
-        className="addToCartBttn"
-        //   onClick={() => addToCart(id)}
-      >
-        Add To Cart
-        {/* {cartItemCount > 0 && <> ({cartItemCount})
-              </>
-              } */}
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
       </button>
     </div>
   );
