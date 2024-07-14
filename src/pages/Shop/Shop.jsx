7483e716209c4c149a8f53c47d049e648ed03167
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-14 04:54:33 +0300

    Update Shop.jsx

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./shop.css";
+import { PRODUCTS } from "../../Products";
+import Product from "./Product";
 
 const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
         <h1>G3RGES Shop</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product
+            key={product.id}
+            name={product.productName}
+            price={product.price}
+            image={product.productImage}
+          />
+        ))}
       </div>
     </div>
   );
