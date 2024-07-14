bc71c982d155f008b89de4bcef714ab3b53a1995
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-15 02:56:13 +0300

    Update Shop.jsx

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -7,7 +7,7 @@ const Shop = () => {
   return (
     <div className="shop">
       <div className="shopTitle">
-        <h1>PedroTech Shop</h1>
+        <h1>G3RGES Shop</h1>
       </div>
 
       <div className="products">
