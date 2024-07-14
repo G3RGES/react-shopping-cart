2acf22f90b20f185a8307436d43d5739ba2b7283
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-14 04:03:15 +0300

    Update Shop.jsx

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./shop.css";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>G3RGES Shop</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
